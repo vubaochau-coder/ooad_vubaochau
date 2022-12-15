df78af05bc85b9775436c055e39cfb82044a0a11
Dinhdu2602 <20520159@gm.uit.edu.vn>
2022-12-15 21:55:27 +0700

    Employee List and Employee's Detail Screen

---
+++ lib/commons/employee_info.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:ooad_vubaochau/commons/radial_progress.dart';
+import 'package:ooad_vubaochau/commons/rounded_image.dart';
+import 'package:ooad_vubaochau/styleguide/text_style.dart';
+
+class MyInfo extends StatelessWidget {
+  const MyInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // ignore: prefer_const_constructors
+          RadialProgress(
+            width: 4,
+            goalCompleted: 0.9,
+            key: null,
+            child: const RoundedImage(
+              imagePath: "images/employee.jpg",
+              size: Size.fromWidth(120.0),
+            ),
+          ),
+
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Vu Bao Chau",
+                style: whiteNameTextStyle,
+              ),
+              Text(
+                ", 20",
+                style: whiteNameTextStyle,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Back-end Developer",
+                style: whiteSubHeadingTextStyle,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
