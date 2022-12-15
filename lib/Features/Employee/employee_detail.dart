432859524e83be0f5c772c4a52a7d7f872678c7b
Dinhdu2602 <82326575+Dinhdu2602@users.noreply.github.com>
2022-12-15 23:31:14 +0700

    Update employee_detail.dart

--- lib/Features/Employee/employee_detail.dart
+++ lib/Features/Employee/employee_detail.dart
@@ -3,80 +3,164 @@ import 'package:ooad_vubaochau/commons/employee_info.dart';
 import 'package:ooad_vubaochau/commons/opaque_image.dart';
 import 'package:ooad_vubaochau/commons/employee_info.dart';
 import 'package:ooad_vubaochau/commons/profile_info_big_card.dart';
+import 'package:ooad_vubaochau/commons/profile_info_card.dart';
 import 'package:ooad_vubaochau/styleguide/colors.dart';
 
 class EmployeeProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        body: Stack(
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Expanded(
-              flex: 4,
-              child: Stack(
-                children: <Widget>[
-                  OpaqueImage(
-                    imageUrl: "images\employee.jpg",
-                  ),
-                  SafeArea(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: const [
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Employee's profile",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: Stack(
+                  children: <Widget>[
+                    OpaqueImage(
+                      imageUrl: "images\employee.jpg",
+                    ),
+                    SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          children: const [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Employee's profile",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
-                          ),
-                          MyInfo(),
-                        ],
+                            MyInfo(),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 5,
-              child: Container(
-                color: Colors.white,
-                child: Table(
-                  children: const [
-                    TableRow(
-                      children: [
-                        ProfileInfoBigCard(
-                          firstText: "25",
-                          secondText: "Dự án đã làm qua",
-                          icon: Icon(Icons.star, color: blueColor),
-                        ),
-                        ProfileInfoBigCard(
-                          firstText: "57 Quang Trung",
-                          secondText: "TP Hồ Chí Minh - Việt Nam",
-                          icon: Image.asset(
-                            "images/adress_icon.png",
-                            color: blueColor,
-                          ),
-                        ),
-                      ],
-                    ),
-                    TableRow(),
-                    TableRow(),
                   ],
                 ),
               ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  padding: const EdgeInsets.only(top: 50),
+                  color: Colors.white,
+                  child: Table(
+                    children: const [
+                      TableRow(
+                        children: [
+                          ProfileInfoBigCard(
+                            firstText: "25",
+                            secondText: "Dự án đã làm qua",
+                            icon: Icon(
+                              Icons.star,
+                              color: blueColor,
+                              size: 32,
+                            ),
+                          ),
+                          ProfileInfoBigCard(
+                            firstText: "57 Quang Trung",
+                            secondText: "TP Hồ Chí Minh - Việt Nam",
+                            icon: Icon(
+                              Icons.add_reaction,
+                              color: blueColor,
+                              size: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          ProfileInfoBigCard(
+                            firstText: "2",
+                            secondText: "Ngày nghỉ",
+                            icon: Icon(
+                              Icons.heart_broken,
+                              color: blueColor,
+                              size: 32,
+                            ),
+                          ),
+                          ProfileInfoBigCard(
+                            firstText: "208",
+                            secondText: "Số ngày đã đi làm",
+                            icon: Icon(
+                              Icons.favorite,
+                              color: blueColor,
+                              size: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          ProfileInfoBigCard(
+                            firstText: "20520159@gm.uit.edu.vn",
+                            secondText: "Email",
+                            icon: Icon(
+                              Icons.email,
+                              color: blueColor,
+                              size: 32,
+                            ),
+                          ),
+                          ProfileInfoBigCard(
+                            firstText: "0336114707",
+                            secondText: "Số điện thoại liên lạc",
+                            icon: Icon(
+                              Icons.headphones,
+                              color: blueColor,
+                              size: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: screenHeight *(4/9),
+            left: 16,
+            right: 16,
+            child: Container(
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  ProfileInfoCard(
+                    firstText: "54%",
+                    secondText: "Progress", imagePath: null,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  ProfileInfoCard(
+                      hasImage: true,
+                      imagePath: "images/pulse_icon.png", firstText: null, secondText: null,
+                  ),
+                  SizedBox(
+                      width: 10,
+                    ),
+                  ProfileInfoCard(
+                      firstText: "Leader team Back-end App",
+                      secondText: "Level", imagePath: null,
+                    ),
+                ],
+              )
             ),
-          ],
-        )
-      ],
-    ));
+          ),
+        ],
+      ),
+    );
   }
 }
