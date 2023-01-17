8f8cf653ffad1cd323de6c4fba1e188fb7e26e6a
Dinhdu2602 <20520159@gm.uit.edu.vn>
2023-01-18 01:12:05 +0700

    MVP Attendance Changes

---
+++ lib/Attendance/abstract_attendance_view.dart
@@ -0,0 +1,4 @@
+abstract class AttendanceView {
+  void getRecord();
+  void getAttendance(String checkIn, String checkOut);
+}
--- lib/Attendance/attendance_model.dart
+++ lib/Attendance/attendance_model.dart
@@ -5,28 +5,35 @@ import '../../Models/Information/User.dart';
 
 class AttendanceModel {
   Future<void> getRecord(String checkIn, String checkOut) async {
-    try {
-      QuerySnapshot snap = await FirebaseFirestore.instance
-          .collection("Account")
-          .where('userName', isEqualTo: User.username)
-          .get();
+    //try {
+    QuerySnapshot snap = await FirebaseFirestore.instance
+        .collection("Account")
+        .where('userName', isEqualTo: User.username)
+        .get();
 
-      DocumentSnapshot snap2 = await FirebaseFirestore.instance
-          .collection("Account")
-          .doc(snap.docs[0].id)
-          .collection("Record")
-          .doc(DateFormat('dd MMMM yyyy').format(DateTime.now()))
-          .get();
-      setState(() {
-        checkIn = snap2['checkIn'];
-        checkOut = snap2['checkOut'];
-      });
-    } catch (e) {
+    DocumentSnapshot snap2 = await FirebaseFirestore.instance
+        .collection("Account")
+        .doc(snap.docs[0].id)
+        .collection("Record")
+        .doc(DateFormat('dd MMMM yyyy').format(DateTime.now()))
+        .get();
+    setState(() {
+      checkIn = snap2['checkIn'];
+      checkOut = snap2['checkOut'];
+    });
+    /*} catch (e) {
       setState(() {
         checkIn = "--/--";
         checkOut = "--/--";
       });
-    }
+    }*/
+  }
+
+  Future<void> getRecordError(String checkIn, String checkOut) async {
+    setState(() {
+      checkIn = "--/--";
+      checkOut = "--/--";
+    });
   }
 
   Future<void> getAttendance(String checkIn, String checkOut) async {
@@ -42,23 +49,23 @@ class AttendanceModel {
         .doc(DateFormat('dd MMMM yyyy').format(DateTime.now()))
         .get();
 
-    try {
-      String checkIn = snap2['checkIn'];
+    //try {
+    String checkIn = snap2['checkIn'];
 
-      setState(() {
-        checkOut = DateFormat('hh:mm').format(DateTime.now());
-      });
+    setState(() {
+      checkOut = DateFormat('hh:mm').format(DateTime.now());
+    });
 
-      await FirebaseFirestore.instance
-          .collection("Account")
-          .doc(snap.docs[0].id)
-          .collection("Record")
-          .doc(DateFormat('dd MMMM yyyy').format(DateTime.now()))
-          .update({
-        'checkIn': checkIn,
-        'checkOut': DateFormat('hh:mm').format(DateTime.now()),
-      });
-    } catch (e) {
+    await FirebaseFirestore.instance
+        .collection("Account")
+        .doc(snap.docs[0].id)
+        .collection("Record")
+        .doc(DateFormat('dd MMMM yyyy').format(DateTime.now()))
+        .update({
+      'checkIn': checkIn,
+      'checkOut': DateFormat('hh:mm').format(DateTime.now()),
+    });
+    /*} catch (e) {
       setState(() {
         checkIn = DateFormat('hh:mm').format(DateTime.now());
       });
@@ -70,7 +77,25 @@ class AttendanceModel {
           .set({
         'checkIn': DateFormat('hh:mm').format(DateTime.now()),
       });
-    }
+    }*/
+  }
+
+  Future<void> getAttendanceError(String checkIn, String checkOut) async {
+    QuerySnapshot snap = await FirebaseFirestore.instance
+        .collection("Account")
+        .where('userName', isEqualTo: User.username)
+        .get();
+    setState(() {
+      checkIn = DateFormat('hh:mm').format(DateTime.now());
+    });
+    await FirebaseFirestore.instance
+        .collection("Account")
+        .doc(snap.docs[0].id)
+        .collection("Record")
+        .doc(DateFormat('dd MMMM yyyy').format(DateTime.now()))
+        .set({
+      'checkIn': DateFormat('hh:mm').format(DateTime.now()),
+    });
   }
 }
 
